5ed0171ffa6175a932e37676757ddb227152176c
Sanjie25 <sanjeev3re@gmail.com>
2025-08-22 16:52:05 +0530

    Added capability of Network hosting and collaboration

--- app/notes.py
+++ app/notes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, flash, render_template, redirect, url_for
 from app.main import db
 from flask_login import current_user, login_required
-from app.database_models import Note, User
+from app.database_models import Note, User, Tag
 from app.blueprints import admin_required
 
 notes_bp = Blueprint("notes", __name__)
@@ -13,6 +13,7 @@ def create_note():
     if request.method == "POST":
         title = request.form["title"]
         content = request.form["content"]
+        tags = request.form["tags"].split()
         error = None
 
         if not title:
@@ -24,6 +25,10 @@ def create_note():
             new_note = Note(title=title, body=content, author_id=current_user.id)
 
             db.session.add(new_note)
+
+            for tag in tags:
+                new_tag = Tag(name=tag)
+
             db.session.commit()
 
             return redirect(url_for("notes.index"))
@@ -36,8 +41,6 @@ def create_note():
 def get_note(note_id):
     note = Note.query.get_or_404(note_id)
     author = db.session.execute(db.select(User).filter_by(id=note.author_id)).fetchone()
-
-    print(author)
 
     return render_template("note_view.html", note=note, author=author)
 
@@ -77,6 +80,7 @@ def edit_note(note_id):
     if request.method == "POST":
         title = request.form["title"]
         content = request.form["content"]
+        tags = request.form["tags"]
         error = None
 
         if not title:
@@ -87,6 +91,9 @@ def edit_note(note_id):
         if error is None:
             note.title = title
             note.body = content
+
+            for tag in tags:
+                new_tag = Tag(name=tag)
 
             db.session.commit()
 
--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from app.main import create_app, db
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app = create_app()
 
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
+
